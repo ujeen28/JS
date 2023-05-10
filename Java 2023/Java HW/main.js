1bceea53382208242f859630477df9bae63ac31d
Ujeen28 <aimgroupzp@gmail.com>
2023-05-10 07:33:42 +0200

    111

---
+++ Java 2023/Java HW/main.js
@@ -0,0 +1,61 @@
+
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999,
+// 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+//
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються
+// вашими Імям, По-Батькові та роками. та вивести в консоль
+
+
+
+let a='Hello'
+console.log(a)
+let b='owu'
+console.log(b)
+let c='com'
+console.log(c)
+let d='ua'
+console.log(d)
+let a1=1
+console.log(a1)
+let b2=10
+console.log(b2)
+let c2=-999
+console.log(c2)
+let d2=123
+console.log(d2)
+let a3=3.14
+console.log(a3)
+let b3=2.7
+console.log(b3)
+let c3=16
+console.log(c3)
+let d3=true
+console.log(d3)
+let a4=false
+console.log(a4)
+
+let firstName='Danchenko'
+let middleName='Ievgen'
+let lastName= 'Oleksaandrovich'
+console.log(firstName, middleName,lastName)
+
+let a7 = 100
+let b8 = '100'
+let c9 = true
+console.log(typeof a7)
+console.log(typeof b8)
+console.log(typeof c9)
+
+
+let name=prompt('Name')
+let sureName=prompt('SureName')
+let lastNam=prompt('Last Name')
+console.log(name, sureName, lastNam)
+
